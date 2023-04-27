cb21aabd6526c7328fc4307666d2d551515becde
Mercy <mercygeorge066@gmail.com>
2023-04-27 07:19:20 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,12 +6,13 @@
  *
  * Return: Always 0
  */
+
 int main(void)
 {
-    int i;
-    i=0;
+        int i;
 
-    positive_or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-    return (0);
+        return (0);
 }
