3b6ef0e392361ab2ee8a48671081cb73ce315098
Mercy <mercygeorge066@gmail.com>
2023-04-27 07:10:10 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Tests for the positive_or_negative function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int zero = 0;
+
+    positive_or_negative(zero);
+
+    return (0);
+}
