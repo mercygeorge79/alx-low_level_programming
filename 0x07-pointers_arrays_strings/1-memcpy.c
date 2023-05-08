0fac4367539d15f844fbef0ba12f7a028751f91e
Mercy <mercygeorge066@gmail.com>
2023-05-08 08:05:45 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,37 +1,23 @@
 #include "main.h"
 
 /**
- * _memset - Fills the first n bytes of the memory area
- * pointed to by s with the constant byte c.
- * @s: Pointer to the memory area to be filled.
- * @c: The byte to fill the memory area with.
- * @n: Number of bytes to be filled.
+ * _memcpy - Copies n bytes from memory area src to memory area dest
+ * @dest: Pointer to destination memory area
+ * @src: Pointer to source memory area
+ * @n: Number of bytes to be copied
  *
- * Return: Pointer to the memory area s.
+ * Return: Pointer to dest
  */
-void *_memset(void *s, int c, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned char *p = s;
 
-	while (n-- > 0)
-		*p++ = (unsigned char)c;
-	return (s);
-}
+	unsigned int i;
 
-/**
- * _memcpy - Copies n bytes from memory area src to memory area dest.
- * @dest: Pointer to the destination array where the content is to be copied.
- * @src: Pointer to the source of data to be copied.
- * @n: Number of bytes to be copied.
- *
- * Return: Pointer to the destination array.
- */
-void *_memcpy(void *dest, const void *src, size_t n)
-{
-	unsigned char *d = dest;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
 
-const unsigned char *s = src;
-	while (n-- > 0)
-		*d++ = *s++;
-	return (dest);
+	}
+
+return (dest);
 }
