7daba15e0e018686e273ea475bc10b9de888a066
Mercy <mercygeorge066@gmail.com>
2023-05-08 08:39:31 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,18 +3,18 @@
 /**
  * _strchr - Locates a character in a string
  * @s: Pointer to the string to be searched
- * @c: Character to be located
+ * @c: The character to be located
  *
  * Return: Pointer to the first occurrence of the character c in the string s,
  *         or NULL if the character is not found
  */
+
 char *_strchr(char *s, char c)
 
 {
 while (*s != '\0')
 
 {
-
 if (*s == c)
 
 {
@@ -26,7 +26,6 @@ return (s);
 s++;
 
 }
-
 if (*s == c)
 
 {
