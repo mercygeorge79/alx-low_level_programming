564e4106c01f563d152764299b20bd6b4174354a
Mercy <mercygeorge066@gmail.com>
2023-05-12 13:17:10 -0700

    3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _strcmp - compares two strings
+  * @s1: input value
+  * @s2: input value
+  * Return: s1 - s2.
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int p = 0;
+
+	while (s1[p] != '\0' && s2[p] != '\0')
+	{
+		if (s1[p] != s2[p])
+		{
+			return (s1[p] - s2[p]);
+		}
+		p++;
+	}
+	return (0);
+}
