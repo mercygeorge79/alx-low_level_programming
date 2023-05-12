060255256f79e12ea52ba49bc05284cf940ba2d3
Mercy <mercygeorge066@gmail.com>
2023-05-12 13:28:45 -0700

    9-strcpy.c

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * char *_strcpy - copies the string including the terminating null
+  * byte (\0), to a buffer
+  *
+  * @dest: buffer copied to
+  * @src: copy from
+  * Return: string
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int p = 0;
+	int q = 0;
+
+	while (*(src + p) != '\0')
+	{
+		p++;
+	}
+	for (; q < p; q++)
+	{
+		dest[q] = src[q];
+	}
+	dest[p] = '\0';
+	return (dest);
+}
