20df84a1bf4726e02f1b5479c3703a36359a1a7f
Mercy <mercygeorge066@gmail.com>
2023-05-12 06:50:49 -0700

    0-whatsmyname.c

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc __attribute__((unused)), char *argv[])`
+/**
+  * main - prints name of a program
+  * @argc: number of the argument
+  * @argv: array
+  * Return: 0.
+  */
+int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n", *argv);
-return (0);
+	printf("%s\n", *argv);
+
+	return (0);
 }
