ab3f31140967a16b7b1d083bc52c3e1f7d95a401
Mercy <mercygeorge066@gmail.com>
2023-05-16 03:33:04 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	i= ci = 0;
+	i = ci = 0;
 
 	while (s1[i] != '\0')
 	i++;
@@ -49,6 +49,5 @@ while (s2[ci] != '\0')
 	conct[i] = s2[ci];
 	i++, ci++;
 }
-conct[i] = '\0';
 	return (conct);
 	}
